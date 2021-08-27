9a0118a461ec0b1cd2dbcf3c12184eb6bf7b2818
Devil-9 <johndoe@example.com>
2021-08-27 13:21:21 +0530

    .

--- lib/screens/loginscreen/loginpage.dart
+++ lib/screens/loginscreen/loginpage.dart
@@ -1,11 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:saman/routs.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool changebutton = false;
+  String name = "";
+
+  final _formkey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formkey.currentState!.validate()) {
+      setState(() {
+        changebutton = true;
+      });
+
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changebutton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/mainlogo.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16.0,
+                horizontal: 32.0,
+              ),
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter username",
+                        labelText: "Username",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Username Can't be empty";
+                        }
+
+                       return null;
+                      },
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password Can't be empty";
+                        }
+                        else if (value.length < 8) {
+                            return "Password Can't be less than 8 digit";
+                        }
+
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+
+                    Material(
+                      color: Colors.deepPurple,
+                      borderRadius: BorderRadius.circular(changebutton ? 50 : 8),
+                      child: InkWell(
+                        splashColor: Colors.red,
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changebutton ? 50 : 150,
+                          height: 40,
+                          alignment: Alignment.center,
+                          child: changebutton
+                              ? Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                        ),
+                      ),
+                    ),
+
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    // )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/splashscreen/splashscreen.dart
+++ lib/screens/splashscreen/splashscreen.dart
@@ -1,11 +1,80 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:saman/routs.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() => new SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  var _visible = true;
+
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  startTime() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.pushNamed(context, MyRoutes.loginRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = new AnimationController(
+        vsync: this, duration: new Duration(seconds: 3));
+    animation =
+        new CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+
+    animation.addListener(() => this.setState(() {}));
+    animationController.forward();
+
+    setState(() {
+      _visible = !_visible;
+    });
+    startTime();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      backgroundColor: HexColor("#000000"),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          // new Column(
+          //   mainAxisAlignment: MainAxisAlignment.end,
+          //   mainAxisSize: MainAxisSize.min,
+          //   children: <Widget>[
+          //     Padding(
+          //         padding: EdgeInsets.only(bottom: 30.0),
+          //         child: new Image.asset(
+          //           'assets/images/powered_by.png',
+          //           height: 25.0,
+          //           fit: BoxFit.scaleDown,
+          //         ))
+          //   ],
+          // ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Image.asset(
+                'assets/images/mainlogo.png',
+                width: animation.value * 250,
+                height: animation.value * 250,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
