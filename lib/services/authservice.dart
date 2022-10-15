ea93673b28dee83dccd4c6ae797c6b0bed4cc71b
Avnish Raj <55361674+Devil-9@users.noreply.github.com>
2022-10-15 23:04:53 +0530

    Update authservice.dart

--- lib/services/authservice.dart
+++ lib/services/authservice.dart
@@ -3,6 +3,12 @@ import 'package:dio/dio.dart';
 class AuthService {
   Dio dio = new Dio();
 
+  
+  sign(email, password) async {
+    return await dio.post('https://saman9.herokuapp.com/authenticate',
+        data: {"email": email, "password": password},
+        options: Options(contentType: Headers.formUrlEncodedContentType));
+  
   login(email, password) async {
     return await dio.post('https://saman9.herokuapp.com/authenticate',
         data: {"email": email, "password": password},
